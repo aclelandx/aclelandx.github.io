046cf1a3802791ed2365d4d1284844b6e59a784c
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-04-07 23:37:40 -0400

    Finalized project for submission

--- src/components/navBar/navBarButton.jsx
+++ src/components/navBar/navBarButton.jsx
@@ -4,10 +4,6 @@ import { Link } from 'react-router-dom';
 function NavButton (props){
     
     return (
-        // <button className={props.cName} onClick={props.action}>
-        //     {props.content}
-            
-        // </button>   
         <Link className={props.content + `btn`} to={props.destination} >
             {props.content}
         </Link> 
